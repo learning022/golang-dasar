ef2ef8fe5ea0b77e1bca66ab540d47c5982102d2
RoniAndarsyah <roni.andarsyah@gmail.com>
2023-04-01 06:21:33 +0700

    compile seluruh pada satu main

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import "fmt"
 
 func main() {
 	// Kasus Pertama Main
-	// fmt.Println("Hello, World! Yuk Belajar Golang Dasar")
-	// sentences := TestAja()
-	// fmt.Println(sentences)
+	fmt.Println("Hello, World! Yuk Belajar Golang Dasar")
+	sentences := TestAja()
+	fmt.Println(sentences)
 
-	// // LOOPING
+	// LOOPING
 	for i := 1; i <= 15; i++ {
 		fmt.Println("Hello, World! Yuk Belajar Golang Dasar Looping : ", i)
+	}
 
+	// Lopping 3
+	title := "Ngoding Golang bersama RPL Class"
+
+	for index, letter := range title {
+		fmt.Println("index :", index, "leter :", string(letter))
 	}
 }
