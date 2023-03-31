5205a24e939275ccd1c7370706a05b9ebe975d1f
RoniAndarsyah <roni.andarsyah@gmail.com>
2023-04-01 06:13:18 +0700

    Tambah File Looping

---
+++ looping1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func Looping1() {
+	// berikut loopingnya pertama
+	for i := 1; i <= 15; i++ {
+		fmt.Println("Hello, World! Yuk Belajar Golang Dasar Looping :", i)
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World! Yuk Belajar Golang Dasar")
-	sentences := TestAja()
-	fmt.Println(sentences)
+	// Kasus Pertama Main
+	// fmt.Println("Hello, World! Yuk Belajar Golang Dasar")
+	// sentences := TestAja()
+	// fmt.Println(sentences)
+
+	// // LOOPING
+	for i := 1; i <= 15; i++ {
+		fmt.Println("Hello, World! Yuk Belajar Golang Dasar Looping : ", i)
+
+	}
 }
